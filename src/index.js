f806bfe6c058be3c4d8e78e80fd629a63f824c58
Jordane Thomas <thomasjordane7@gmail.com>
2017-08-07 16:29:17 -0400

    File Dump of Everything
    This is the project thus far

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App source="http://ec2-13-59-71-65.us-east-2.compute.amazonaws.com/api/events" />, document.getElementById('root'));
+registerServiceWorker();
