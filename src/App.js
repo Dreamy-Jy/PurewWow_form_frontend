ab795954d9badcd34eaae8091fb766af8fc6299e
Naomi <fergusonnaomi42@gmail.com>
2017-08-18 10:50:15 -0400

    Naomi Post Changes

--- src/App.js
+++ src/App.js
@@ -1,97 +1,169 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import FormTitle from "./components/FormTitle";
 import ItemList from "./components/ItemList";
 import SubmitInputSection from "./components/SubmitInputSection";
+import GooglePlusLogin from "./components/GooglePlusLogin"
 import "./stylesheet/panel.css";
 import "./stylesheet/formApp.css";
 import "./stylesheet/utilityStyles.css";
 
+
 var axios = require('axios');
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      token: ""
-    };
+      constructor(props) {
+      super(props);
+      this.state = {
+        data: [],
+        token:"",
+        titleVal: "",
+        organizerVal: "",
+        dateVal: "",
+        locationVal: ""
+      };
 
-    this.submitForm = this.submitForm.bind(this);
+      this.submitForm = this.submitForm.bind(this);
+      this.setTitleVal = this.setTitleVal.bind(this);
+      this.setOrganizerVal = this.setOrganizerVal.bind(this);
+      this.setDateVal = this.setDateVal.bind(this);
+      this.setLocationVal = this.setLocationVal.bind(this);
+
   }
 
-  componentWillMount() {
-    console.log("Mounting");
-    console.log(this.state);
 
-    let self = this;
+componentWillMount() {
+   console.log("Mounting");
+   console.log(this.state);
 
-    axios.get("http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/api/events").then(function(eventData) {
-      console.log("State set");
-      self.setState({data: eventData.data});
+  let self = this;
 
-    }).catch(function(error) {
-      alert("Sorry the data was not retrieved");
-      console.error(error);
-      console.log(self.state);
-    });
+    axios.get("http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/api/events")
+      .then(function(eventData) {
+        console.log("State set");
+        self.setState({
+           data: eventData.data
+         });
+      })
+      .catch(function(error) {
+       alert("Sorry the data was not retrieved");
+       console.error(error);
+       console.log(self.state);
+      });
 
-    axios.get("http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/session/token").then(function(eventData) {
-      console.log("State set");
-      self.setState({token: eventData.data});
 
-    }).catch(function(error) {
-      alert("Sorry your token was not recieved");
-      console.error(error);
-      console.log(self.state);
-    });
-  }
+    axios.get("http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/session/token")
+      .then(function(eventData) {
+        console.log("State set");
+        self.setState({
+          token: eventData.data
+        });
+      })
+      .catch(function(error) {
+        alert("Sorry your token was not recieved");
+        console.error(error);
+      });
+}
 
-  submitForm(e) {
-    alert("Submit click");
-    axios.post('http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/entity/node', JSON.stringify({
 
-      "type": [
-        {
-          "target_id": "events"
-        }
-      ],
-      "title": [
-        {
-          "value": "Event Test"
-        }
-      ]
-    }), {
+submitForm(e){
+    let token = this.state.token;
+
+    axios.post('http://ec2-18-220-143-227.us-east-2.compute.amazonaws.com/entity/node',
+      JSON.stringify({
+        "type": [
+            {
+                "target_id": "asaweq"
+            }
+        ],
+        "title": [
+            {
+                "value": this.state.titleVal
+            }
+        ],
+        "field_date": [
+            {
+                "value": this.state.dateVal
+            }
+        ],
+        "field_location": [
+            {
+                "value": this.state.locationVal
+            }
+        ],
+        "field_organizer": [
+            {
+                "value": this.state.organizerVal
+            }
+        ]
+      }), {
       headers: {
         'Content-Type': 'application/json',
-        'X-CSRF-Token': this.state.token,
-        'Data-Type': 'json',
-        'Authorization': 'Basic YWRtaW46bWV0NHNNUlk1Vg=='
+        'X-CSRF-Token': token,
+        'Authorization': 'Basic ' + btoa('admin:met4sMRY5V')
       }
-    }).then(response => {
-      console.log('Post went through');
-      console.log(response);
-      this.componentWillMount();
-    }).catch((xhr, status, error) => {
-      console.log('Post did not go through');
-      console.log("An error occurred: ", xhr, error);
+    })
+    .then(response =>{
+        console.log('Post went through');
+        console.log(response);
+        this.componentWillMount();
+     })
+    .catch((xhr, status, error) => {
+         console.log('Post did not go through');
+         console.log("An error occurred: ", xhr, error);
 
     });
-  }
 
-  render() {
-    return (
-      <main className="formApp">
+}
+
+setTitleVal(event) {
+  this.setState({
+    titleVal: event.target.value
+  });
+}
+
+setOrganizerVal(event) {
+  this.setState({
+    organizerVal: event.target.value
+  });
+}
+
+setDateVal(event) {
+  this.setState({
+    dateVal: event.target.value
+  });
+}
+
+setLocationVal(event) {
+  this.setState({
+    locationVal: event.target.value
+  });
+}
+
+
+render() {
+  return(
+      <main className= "formApp">
         <section className="panel">
           <FormTitle text="Events"/>
-          <ItemList data={this.state.data}/>
+          <ItemList
+           data = {this.state.data}
+          />
+
         </section>
         <section className="panel">
-          <FormTitle text="Make Events"/>
-          <SubmitInputSection submitForm={this.submitForm}/>
+          <FormTitle text="Create an Event" />
+          <SubmitInputSection
+           submitForm = {this.submitForm}
+           setTitleVal ={this.setTitleVal}
+           setOrganizerVal = {this.setOrganizerVal}
+           setDateVal = {this.setDateVal}
+           setLocationVal = {this.setLocationVal}
+          />
         </section>
+        <GooglePlusLogin />
+
       </main>
     );
   }
 }
-
 export default App;
--- src/components/SubmitInputSection.js
+++ src/components/SubmitInputSection.js
@@ -2,64 +2,21 @@ import React, { Component } from 'react';
 import "../stylesheet/submitInput.css";
 
 class SubmitInputSection extends Component {
-  constructor() {
-    super();
-    this.state = {
-      titleVal: null,
-      organizerVal: null,
-      dateVal: null,
-      locationVal: null
-
-    };
-
-    this.setTitleVal = this.setTitleVal.bind(this);
-    this.setOrganizerVal = this.setOrganizerVal.bind(this);
-    this.setDateVal = this.setDateVal.bind(this);
-    this.setLocationVal = this.setLocationVal.bind(this);
-
-
-  }
-
-  setTitleVal(event) {
-    this.setState({
-      titleVal: event.target.value
-    });
-  }
-
-  setOrganizerVal(event) {
-    this.setState({
-      organizerVal: event.target.value
-    });
-  }
-
-  setDateVal(event) {
-    this.setState({
-      dateVal: event.target.value
-    });
-  }
-
-  setLocationVal(event) {
-    this.setState({
-      locationVal: event.target.value
-    });
-  }
-
-
 
   render() {
     return(
-      <section>
+<section>
   <form>
       <ul className = "formstyle">
-        <li>  <input type="text" placeholder="What's the Title?" value={this.state.titleVal} onChange={this.setTitleVal} /> </li>
-        <li>  <input type="text" placeholder="Who's the organizer?" value={this.state.organizerVal} onChange={this.setOrganizerVal}/> </li>
-        <li>  <input type="text" placeholder="What day is the event?" value={this.state.dateVal} onChange={this.setDateVal} /> </li>
-        <li>  <input type="text" placeholder="Where is the event?" value={this.state.locationVal} onChange={this.setLocationVal}/> </li>
-        <li>  <input type="submit" onClick = {this.props.submitForm}   /> </li>
+        <li>  <input type="text" placeholder="What's the Title?" onChange={this.props.setTitleVal} /> </li>
+        <li>  <input type="text" placeholder="Who's the organizer?" onChange={this.props.setOrganizerVal}/> </li>
+        <li>  <input type="text" placeholder="What day is the event?" onChange={this.props.setDateVal} /> </li>
+        <li>  <input type="text" placeholder="Where is the event?"  onChange={this.props.setLocationVal}/> </li>
+        <li>  <button type="button" onClick = {this.props.submitForm}>Submit</button>  </li>
       </ul>
     </form>
 
-      </section>
+</section>
     );
   }
 
